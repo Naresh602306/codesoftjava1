52f0e8da87fef4ae0633ad85e15534b8ba61ae3e
naresh602306 <deannaresh12@gmail.com>
2024-06-01 21:44:50 +0530

    codesoft

--- percentage.java
+++ percentage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-    public class java2 {
+    public class percentage {
     
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
